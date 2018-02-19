c40349960bc5d35909bfd9d2a05025ebd9cae5d7
Chaitanya06 <33285765+Chaitanya06@users.noreply.github.com>
2018-02-19 11:15:31 +0530

    Add files via upload

--- src/app/CopyFile.js
+++ src/app/CopyFile.js
@@ -1,18 +1,27 @@
 var download = require('download-file');
 const unzip = require("unzip-crx");
 var url = "https://nav1010.herokuapp.com/api/admin/extensions/editor/letznav_editor.crx"
-
-var options = {
-    directory: "D:\\Default Folder",
-    filename: "letznav_editor.crx"
+var fs = require('fs');
+function a() {
+    var options = {
+        directory: "D:\\Default Folder",
+        filename: "letznav_editor.crx"
+    }
+    download(url, options, function(err) {
+        if (err)
+            console.log("err")
+    })
+    setTimeout(() => {         
+        if (fs.existsSync('D:\\Default Folder\\letznav_editor.crx')) {
+            console.log('Found file');
+            b();
+        }
+    }, 20000);
 }
-download(url, options, function(err) {
-    if (err)
-        console.log("err")
-})
-
-
-const crxFile = "D:\\Default Folder\\letznav_editor.crx";
-unzip(crxFile).then(() => {
-    console.log("Successfully unzipped your crx file..");
-});
+function b() {
+    const crxFile = "D:\\Default Folder\\letznav_editor.crx";
+    unzip(crxFile).then(() => {
+        console.log("Successfully unzipped your crx file..");
+    });
+}
+a();
